799b919857eada86a01a3d7d949d2d0987fad616
Lisandro Civili <lisandrocivili13@gmail.com>
2024-02-04 17:31:53 -0300

    Maquetado principal y logica finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Titulo from './components/Titulo';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    apellido: '',
+    dni: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.nombre && formData.apellido && formData.dni && formData.email) {
+      alert('Datos enviados');
+    } else {
+      alert('Completar todos los datos');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <Titulo/>
+      <form className="border border-top-0 px-5 py-3" onSubmit={handleSubmit}>
+        <h5 className='py-4'>Llenar el formulario para crear una cita</h5>
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Nombre</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="Ingrese su nombre" name="nombre" onChange={handleChange} />
+          </div>
+        </div>
 
-export default App
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Apellido</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="Ingrese su apellido" name="apellido" onChange={handleChange} />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">DNI</label>
+          <div className="col-sm-10">
+            <input type="text" className="form-control" placeholder="Ingrese su DNI" name="dni" onChange={handleChange} />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" placeholder="Ingrese su email" name="email" onChange={handleChange} />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-10 offset-sm-2 text-center">
+            <button type="submit" className="btn btn-primary btn-lg">
+              Enviar
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Titulo = () => {
+    return (
+        <h1 className='text-center py-4 bg-info-subtle'>Adminsitrador pacientes veterinaria</h1>
+    );
+};
+
+export default Titulo;
